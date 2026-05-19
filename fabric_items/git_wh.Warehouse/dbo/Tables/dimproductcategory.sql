58d2169db5e505582cd78decffc67f8fc89e4ad4
Mani Singh <realmanisingh@gmail.com>
2026-05-19 15:12:41 -0400

    Committing 9 items from workspace 13f10688-7797-4c03-88ed-477906154e1d

---
+++ fabric_items/git_wh.Warehouse/dbo/Tables/dimproductcategory.sql
@@ -0,0 +1,8 @@
+CREATE TABLE [dbo].[dimproductcategory] (
+
+	[ProductCategoryKey] int NULL, 
+	[ProductCategoryAlternateKey] int NULL, 
+	[EnglishProductCategoryName] varchar(8000) NULL, 
+	[SpanishProductCategoryName] varchar(8000) NULL, 
+	[FrenchProductCategoryName] varchar(8000) NULL
+);
