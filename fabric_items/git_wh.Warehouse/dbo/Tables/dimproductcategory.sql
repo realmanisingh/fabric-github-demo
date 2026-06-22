d82b505a23bf599d60672361f892ffe49dcfeb01
Mani Singh <realmanisingh@gmail.com>
2026-06-22 12:03:57 -0400

    Delete fabric_items directory

--- fabric_items/git_wh.Warehouse/dbo/Tables/dimproductcategory.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE [dbo].[dimproductcategory] (
-
-	[ProductCategoryKey] int NULL, 
-	[ProductCategoryAlternateKey] int NULL, 
-	[EnglishProductCategoryName] varchar(8000) NULL, 
-	[SpanishProductCategoryName] varchar(8000) NULL, 
-	[FrenchProductCategoryName] varchar(8000) NULL
-);
